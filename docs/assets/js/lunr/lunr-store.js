269faca966874c0363a8447973f7b3e57705865d
Dominik Kornas <dominik.kornas@vml.com>
2025-10-07 16:38:25 +0200

    cleanup

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,7 +1,1 @@
-var store = [{
-        "title": "Layout: Header Image (Horizontal)",
-        "excerpt":"This post should display a header image, if the theme supports it.   Non-square images can provide some unique styling issues.   This post tests a horizontal header image.  ","categories": ["Layout","Uncategorized"],
-        "tags": ["edge case","featured image","image","layout"],
-        "url": "/layout/uncategorized/2012/03/15/layout-header-image-horizontal.html",
-        "teaser": null
-      }]
+var store = []
