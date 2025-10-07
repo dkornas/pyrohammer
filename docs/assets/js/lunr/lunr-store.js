243ad5647a88f248a8ecc79249dbe21d74b611a0
Dominik Kornas <dominik.kornas@vml.com>
2025-10-07 19:46:05 +0200

    Site skeleton

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,1 +1,31 @@
-var store = []
+var store = [{
+        "title": "Witamy!",
+        "excerpt":"Strona turnieju Pyrohammer 2025 rusza od dziś. Aktualności i informacje na temat spraw organizacyjnych, lokalizacji i całej logistyki pojawiaą się wkrótce.     Niebawem aktywny będzie także link do ChampionsHub gdzie oficnalnie ruszą zapisy na turniej. Link można znaleźć na stronie głównej.   ","categories": ["News"],
+        "tags": ["News","Updates"],
+        "url": "/news/witamy/",
+        "teaser": null
+      },{
+    "title": "Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist.  ","url": "https://pyrohammer.pl/404.html"
+  },{
+    "title": "Aktualności",
+    "excerpt":" ","url": "https://pyrohammer.pl/aktualnosci/"
+  },{
+    "title": "Atrakcje",
+    "excerpt":"Atrakcje dla osób towarzyszących. Co robić podczas splita. Wife pointy.  ","url": "https://pyrohammer.pl/atrakcje/"
+  },{
+    "title": "Comp turniejowy",
+    "excerpt":"Comp turniejowy.  ","url": "https://pyrohammer.pl/comp/"
+  },{
+    "title": "Lokalizacja",
+    "excerpt":"Lorem ipsum  ","url": "https://pyrohammer.pl/lokalizacja/"
+  },{
+    "title": "Modelarka",
+    "excerpt":"Ocena modelarska. Konkurs malowania. ","url": "https://pyrohammer.pl/modelarka/"
+  },{
+    "title": "Organizatorzy",
+    "excerpt":"Strefa Zero Poznań  Dominik Kornaś \"Myzu\" Radosław Woźniak \"James\" ","url": "https://pyrohammer.pl/organizatorzy/"
+  },{
+    "title": "Terminarz",
+    "excerpt":"Plan turnieju. Godziny rozpoczęcia rund.","url": "https://pyrohammer.pl/terminarz/"
+  }]
