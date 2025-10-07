300779751a5a991a4cab9446401c8510e3718864
Dominik Kornas <dominik.kornas@vml.com>
2025-10-07 19:57:09 +0200

    Site skeleton

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Witamy!",
-        "excerpt":"Strona turnieju Pyrohammer 2025 rusza od dziś. Aktualności i informacje na temat spraw organizacyjnych, lokalizacji i całej logistyki pojawiaą się wkrótce.     Niebawem aktywny będzie także link do ChampionsHub gdzie oficnalnie ruszą zapisy na turniej. Link można znaleźć na stronie głównej.   ","categories": ["News"],
+        "excerpt":"Strona turnieju Pyrohammer 2025 rusza od dziś. Aktualności i informacje na temat spraw organizacyjnych, lokalizacji i całej logistyki pojawią się wkrótce.     Niebawem aktywny będzie także link do ChampionsHub gdzie oficnalnie ruszą zapisy na turniej. Link można znaleźć na stronie głównej.   ","categories": ["News"],
         "tags": ["News","Updates"],
         "url": "/news/witamy/",
         "teaser": null
@@ -17,14 +17,17 @@ var store = [{
     "title": "Comp turniejowy",
     "excerpt":"Comp turniejowy.  ","url": "https://pyrohammer.pl/comp/"
   },{
+    "title": "Galeria",
+    "excerpt":"Galeria.  ","url": "https://pyrohammer.pl/galeria/"
+  },{
     "title": "Lokalizacja",
-    "excerpt":"Lorem ipsum  ","url": "https://pyrohammer.pl/lokalizacja/"
+    "excerpt":"Lokalizacja turnieju.  ","url": "https://pyrohammer.pl/lokalizacja/"
   },{
     "title": "Modelarka",
     "excerpt":"Ocena modelarska. Konkurs malowania. ","url": "https://pyrohammer.pl/modelarka/"
   },{
     "title": "Organizatorzy",
-    "excerpt":"Strefa Zero Poznań  Dominik Kornaś \"Myzu\" Radosław Woźniak \"James\" ","url": "https://pyrohammer.pl/organizatorzy/"
+    "excerpt":"Strefa Zero Poznań  Dominik Kornaś \"Myzu\"  Radosław Woźniak \"James\" ","url": "https://pyrohammer.pl/organizatorzy/"
   },{
     "title": "Terminarz",
     "excerpt":"Plan turnieju. Godziny rozpoczęcia rund.","url": "https://pyrohammer.pl/terminarz/"
